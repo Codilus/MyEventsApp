9f0ea249bf9237bdf7c09561a36df8b2ce26b955
Jun <felipe.jun.ohira@gmail.com>
2016-03-04 19:33:11 -0300

    Creating the budget request when selecting a promoter

--- src/js/controllers/contract/promoter.ctrl.js
+++ src/js/controllers/contract/promoter.ctrl.js
@@ -3,12 +3,20 @@
 
   angular.module('MyEventsApp')
 	  .controller('ContractPromoterCtrl',
-			function($scope, $stateParams, Promoter) {
+			function($scope, $stateParams, Promoter, BudgetResource) {
 
-        Promoter.get($stateParams.promoterId).then(setPromoter);
+        Promoter.get($stateParams.promoterId)
+          .then(setPromoter);
 
         function setPromoter(promoter) {
           $scope.promoter = promoter;
+        }
+
+        $scope.requestBudget = function() {
+          var budget = new BudgetResource();
+          budget.clientId = 1;
+          budget.promoterId = $scope.promoter.id;
+          budget.$save();
         }
 
 			});
