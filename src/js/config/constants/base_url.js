bba822726d415a6979e818c4de95f5ce27417440
Jun <felipe.jun.ohira@gmail.com>
2016-04-14 22:08:09 -0300

    Changing the ionic config files

--- src/js/config/constants/base_url.js
+++ src/js/config/constants/base_url.js
@@ -3,7 +3,7 @@
 
   angular.module('MyEventsApp')
 	  .constant('BaseUrl', (function() {
-      var DEVELOPMENT_URL = 'http://localhost:3000';
+      var DEVELOPMENT_URL = 'http://127.0.0.1:3000';
       var PRODUCTION_URL = 'https://rocky-hamlet-43434.herokuapp.com';
 
 			return {
--- src/js/config/ionic.js
+++ src/js/config/ionic.js
@@ -3,15 +3,19 @@
 
   angular.module('MyEventsApp')
   .run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        cordova.plugins.Keyboard.disableScroll(true);
-
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
+    $ionicPlatform.ready(function loadPlugins() {
+      try {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+          cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+          // org.apache.cordova.statusbar required
+          StatusBar.styleDefault();
+        }
+      } catch (e) {
+        console.log(e);
+        setTimeout(loadPlugins, 200);
       }
     });
   });
